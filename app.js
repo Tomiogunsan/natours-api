85ce7aae197490991e285c4defd0a1f84bd8b639
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-09-03 14:11:45 +0100

    post request

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const express = require('express')
+const fs = require('fs');
+const express = require('express');
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).send('Hello World!'))
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
 
-const port = 3000
+// app.get('/', (req, res) => res.status(200).send('Hello World!'))
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body)
+    res.send('Done')
+})
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
