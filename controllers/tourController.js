adb2e0f0da3a9ca7cc5b4363c45da9c376e5059b
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-09-08 15:02:04 +0100

    implemented pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -51,6 +51,10 @@ exports.getAllTours = async (req, res) => {
 
     const skip = (page - 1) * limit;
     query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
 
     // execute the query
     const tours = await query;
