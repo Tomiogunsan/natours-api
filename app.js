372defdd10241f9b61dea320360e25803e02d438
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-09-09 16:46:21 +0100

    created global error handling middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 app.use(express.json());
@@ -22,24 +24,13 @@ app.use('/api/v1/users', userRouter);
 
 // handling unhandled routes
 app.all('*', (req, res, next) => {
-  //   res.status(404).json({
-  //     status: 'fail',
-  //     message: `can't find ${req.originalUrl} on this server`,
-  //   });
-  const err = new Error(`can't find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  //   const err = new Error(`can't find ${req.originalUrl} on this server`);
+  //   err.status = 'fail';
+  //   err.statusCode = 404;
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
 });
 
 // global error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
