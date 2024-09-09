a87615dc9f75198961f9b74f9ff45113bb2d0909
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-09-09 15:58:41 +0100

    created global error handling middleware

--- app.js
+++ app.js
@@ -21,10 +21,24 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // handling unhandled routes
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `can't find ${req.originalUrl} on this server`,
+app.all('*', (req, res, next) => {
+  //   res.status(404).json({
+  //     status: 'fail',
+  //     message: `can't find ${req.originalUrl} on this server`,
+  //   });
+  const err = new Error(`can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+// global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
